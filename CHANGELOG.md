a863d75c96d37aa2e0894fe360bf80989fab379e
Nazmul Alam <nazmulpcc@gmail.com>
2023-01-26 22:02:46 +0600

    fix facade autocomplettion & basic housekeeping

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,10 @@
 # Changelog
 
+# 0.1.1
+- Update facade autocompletion
+- Update Readme
+
+# 0.1.0
+- Initial release
+- Added child merchant creatio
+- Added order creation, tracking & cancellation
--- src/Facades/Paperfly.php
+++ src/Facades/Paperfly.php
@@ -3,9 +3,17 @@
 namespace Nazmul\Paperfly\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Nazmul\Paperfly\DTO\MerchantRegistrationDto;
+use Nazmul\Paperfly\DTO\MerchantRegistrationResponse;
+use Nazmul\Paperfly\DTO\OrderCreationRequestDto;
+use Nazmul\Paperfly\DTO\OrderResponseDto;
 
 /**
  * @see \Nazmul\Paperfly\Paperfly
+ * @method static MerchantRegistrationResponse registerChildMerchant(MerchantRegistrationDto $data)
+ * @method static OrderResponseDto createOrder(OrderCreationRequestDto $data)
+ * @method static array trackOrder(string $orderReference, string $merchantCode)
+ * @method static array cancelOrder(string $orderReference, string $merchantCode)
  */
 class Paperfly extends Facade
 {
